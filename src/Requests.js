3a0ed8560da11ae7f5877f0f46bf1bcd3b5d34d5
adityayusman <122957438+adityayusman@users.noreply.github.com>
2023-02-27 09:41:36 +0700

    Update Requests.js

--- src/Requests.js
+++ src/Requests.js
@@ -1,4 +1,4 @@
-const key = "b9016ddfe3dee843fd2bc55508cc3b84";
+const key = "";
 
 const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
